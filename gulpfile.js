7a1e62923ec1c890e672ee7346c0411d1c134f63
Luke Bertram <lukebertram@gmail.com>
2018-01-07 17:12:09 -0800

    point jsBrowserify task to correct tmp/ dir

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,20 @@ var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
 var del = require('del');
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
+var lib = require('bower-files') ({
+ "overrides" : {
+   "bootstrap" : {
+     "main": [
+       "less/bootstrap.less",
+       "dist/css/bootstrap.css",
+       "dist/js/bootstrap.js"
+     ]
+   }
+ }
+});
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 //GULP TASKS
@@ -16,7 +30,7 @@ var jshint = require('gulp-jshint');
 //take concatenated JS file from tmp/, transpile from ES6, browserify
   //and place in build/js/ directory
 gulp.task('jsBrowserify', ['concatInterface'], function() {
-  return browserify({ entries: ['.tmp/allConcat.js']})
+  return browserify({ entries: ['./tmp/allConcat.js']})
     .transform(babelify.configure({
       presets: ["es2015"]
     }))
@@ -50,4 +64,71 @@ gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bower', ['bowerJS', 'cssConcat']);
+
+gulp.task('bowerJS', function() {
+ return gulp.src(lib.ext('js').files)
+   .pipe(concat('vendor.min.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function(){
+ return gulp.src(lib.ext('css').files)
+   .pipe(concat('vendor.css'))
+   .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('cssConcat', ['bowerCSS', 'cssBuild'], function() {
+ return gulp.src(['./build/css/vendor.css', './build/css/main.css'])
+   .pipe(concat('build.css'))
+   .pipe(gulp.dest('./build/css'))
+   .pipe(browserSync.stream());
+});
+
+gulp.task('cssBuild', function() {
+ return gulp.src('./scss/main.scss')
+   .pipe(sourcemaps.init())
+   .pipe(sass().on('error', sass.logError))
+   .pipe(sourcemaps.write())
+   .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task('build', ['clean'], function(){
+ if (buildProduction) {
+   gulp.start('minifyScripts');
+ } else {
+   gulp.start('jsBrowserify');
+ }
+ gulp.start('bower');
+});
+
+
+gulp.task('serve', function() {
+ browserSync.init({
+   server: {
+     baseDir: "./",
+     index: "index.html"
+   }
+ });
+
+ gulp.watch(['js/*.js'], ['jsBuild']);
+ gulp.watch(['bower.json'], ['bowerBuild']);
+ gulp.watch(['*.html'], ['htmlBuild']);
+ gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['cssConcat']);
+
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+ browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+ browserSync.reload();
+});
+
+gulp.task('htmlBuild', function(){
+ browserSync.reload();
 });
